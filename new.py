c7f5f9a398c76967ac9b05fedfbfd1f523a7f7b8
shubhambhoj <shubhambhoj75@gmail.com>
2022-09-10 10:58:03 +0530

    add new file

---
+++ new.py
@@ -0,0 +1,16 @@
+# no=int(input("enter no:"))
+# while no!=5 and no<=10:
+#     print(no,no*no)
+#     no=int(input("enter no:"))
+# else:
+#     if no==5:
+#         print("no is 5")
+#     else:    
+#         print(no,"is greater than 10")    
+s="mumbai"
+Ist=["desert","dessert","to","too","lose","loose"]
+tpl=(10,20,30,-20,-10,60)
+i=0
+while i < len(Ist):
+    print(i,s[i],Ist[i],tpl[i])
+    i+=1
